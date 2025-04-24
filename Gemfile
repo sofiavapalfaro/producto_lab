c83661cbb03fed42182f8ca6420a264d6b342f44
Sofia Alfaro <sofiavapalfaro98@gmail.com>
2025-04-24 08:21:10 -0500

    nuevo proyecto y configuaraciones iniciales

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ group :test do
 end
 
 gem "ostruct", "~> 0.6.1"
+
+gem "devise", "~> 4.9"
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,6 +30,8 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
 
